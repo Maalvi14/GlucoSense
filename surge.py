cc59810a076e04caad2e44c8934e8ed98e76b267
Manuel Villanueva <118570103+Maalvi14@users.noreply.github.com>
2023-07-09 13:56:59 -0500

    Update surge.py

--- surge.py
+++ surge.py
@@ -11,19 +11,37 @@ timestamps = pd.to_datetime(data['timestamps'])
 # Detect spikes in glucose levels
 threshold = 1.5  # Adjust this threshold based on your data
 
-spike_timestamps = []
+spike_hours = []
 spike_glucose_levels = []
 for i in range(1, len(glucose_levels) - 1):
     if glucose_levels[i] - glucose_levels[i - 1] > threshold and glucose_levels[i] - glucose_levels[i + 1] > threshold:
-        spike_timestamps.append(timestamps[i])
+        spike_hours.append(timestamps[i].hour)
         spike_glucose_levels.append(glucose_levels[i])
 
-# Plot the spikes in glucose levels
-plt.plot(timestamps, glucose_levels, label='Glucose Levels')
-plt.scatter(spike_timestamps, spike_glucose_levels, color='red', label='Spikes')
-plt.xlabel('Timestamp')
-plt.ylabel('Glucose Levels')
-plt.title('Glucose Level Spikes')
-plt.legend()
-plt.xticks(rotation=45)
+# Group spikes by hour
+spikes_by_hour = {}
+for hour, glucose in zip(spike_hours, spike_glucose_levels):
+    if hour in spikes_by_hour:
+        spikes_by_hour[hour].append(glucose)
+    else:
+        spikes_by_hour[hour] = [glucose]
+
+# Calculate average spikes per hour
+average_spikes = {}
+for hour, spikes in spikes_by_hour.items():
+    average_spikes[hour] = sum(spikes) / len(spikes)
+
+# Create a list of hours in a day
+hours_in_day = list(range(24))
+
+# Create a list of average spike levels for each hour
+average_spike_levels = [average_spikes.get(hour, 0) for hour in hours_in_day]
+
+# Plot the average spike levels
+plt.plot(hours_in_day, average_spike_levels, marker='o')
+plt.xlabel('Hour of Day')
+plt.ylabel('Average Glucose Level Spikes')
+plt.title('Expected Average Glucose Level Spikes in a Normal Day')
+plt.xticks(range(0, 24, 2))  # Set x-axis tick positions at every 2 hours
+plt.grid(True, linestyle='--', alpha=0.5)  # Add grid lines
 plt.show()
