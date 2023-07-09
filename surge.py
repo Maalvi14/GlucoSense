e3d1dca6e257f79c0d0da0128a8a3f3d8c4342e0
Manuel Villanueva <118570103+Maalvi14@users.noreply.github.com>
2023-07-09 13:32:16 -0500

    Update surge.py
    Fixed code structure to show individual days

--- surge.py
+++ surge.py
@@ -13,31 +13,34 @@ glucose_levels = (glucose_levels - np.mean(glucose_levels)) / np.std(glucose_lev
 # Detect spikes in glucose levels
 threshold = 1.5  # Adjust this threshold based on your data
 
-spike_times = []
+spike_times_by_day = {}
 for i in range(1, len(glucose_levels) - 1):
     if glucose_levels[i] - glucose_levels[i - 1] > threshold and glucose_levels[i] - glucose_levels[i + 1] > threshold:
-        spike_times.append(timestamps[i])
+        day = timestamps[i].date()
+        time = timestamps[i].time()
+        if day in spike_times_by_day:
+            spike_times_by_day[day].append(time)
+        else:
+            spike_times_by_day[day] = [time]
 
-# Create a plot to visualize the spikes
-fig, ax = plt.subplots()
-ax.plot(timestamps, glucose_levels, label='Glucose Levels')
-ax.scatter(spike_times, [glucose_levels[timestamps.tolist().index(time)] for time in spike_times],
-           color='red', label='Spikes')
-ax.set_xlabel('Timestamp')
-ax.set_ylabel('Glucose Levels')
-ax.set_title('Glucose Level Spikes')
-ax.legend()
+# Plot the spikes in glucose levels for each day
+for day, spike_times in spike_times_by_day.items():
+    fig, ax = plt.subplots()
+    day_timestamps = [timestamps[i] for i in range(len(timestamps)) if timestamps[i].date() == day]
+    day_glucose_levels = [glucose_levels[i] for i in range(len(timestamps)) if timestamps[i].date() == day]
+    ax.plot(day_timestamps, day_glucose_levels, label='Glucose Levels')
+    ax.scatter([timestamps[timestamps.tolist().index(time)] for time in spike_times],
+               [glucose_levels[timestamps.tolist().index(time)] for time in spike_times],
+               color='red', label='Spikes')
+    ax.set_xlabel('Timestamp')
+    ax.set_ylabel('Glucose Levels')
+    ax.set_title(f'Glucose Level Spikes - {day}')
+    ax.legend()
+    fig.autofmt_xdate()
+    plt.show()
 
-# Format x-axis timestamps
-fig.autofmt_xdate()
-
-# Show the plot
-plt.show()
-
-# Print the times of spikes
-if len(spike_times) > 0:
-    print("Spikes in glucose levels were detected at the following times:")
+    print(f"Spikes in glucose levels for {day} were detected at the following times:")
     for time in spike_times:
-        print(time.time())
-else:
-    print("No spikes in glucose levels were detected.")
+        print(time)
+
+    print("--------------------")
